614fe20a19514f7cbd2aa8fc78dbb9a58e97d249
adiguno <guodian44@gmail.com>
2022-04-23 21:10:52 -0400

    add som color scheme and padding

--- doodles-n-photos/src/App.js
+++ doodles-n-photos/src/App.js
@@ -3,16 +3,38 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <h1>Doodles and Photos</h1>
+      </div>
+
       <div className="grid-container">
-        <img src={require("./assets/1.JPG")} alt="1" />
-        <img src={require("./assets/2.PNG")} alt="2" />
-        <img src={require("./assets/3.PNG")} alt="3" />
-        <img src={require("./assets/4.PNG")} alt="4" />
-        <img src={require("./assets/5.PNG")} alt="5" />
-        <img src={require("./assets/6.PNG")} alt="6" />
-        <img src={require("./assets/7.PNG")} alt="7" />
-        <img src={require("./assets/8.PNG")} alt="8" />
-        <img src={require("./assets/9.png")} alt="9" />
+        <div className="image-card">
+          <img src={require("./assets/1.JPG")} alt="1" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/2.PNG")} alt="2" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/3.PNG")} alt="3" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/4.PNG")} alt="4" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/5.PNG")} alt="5" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/6.PNG")} alt="6" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/7.PNG")} alt="7" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/8.PNG")} alt="8" />
+        </div>
+        <div className="image-card">
+          <img src={require("./assets/9.png")} alt="9" />
+        </div>
       </div>
     </div>
   );
